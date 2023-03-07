78e9c4eea34b3ca6dd6cfafee859d96443fac4e0
ZoyavanMeel <109024871+ZoyavanMeel@users.noreply.github.com>
2023-03-07 22:29:48 +0100

    create skeleton for all functions to test

--- Test/test_ORCA.py
+++ Test/test_ORCA.py
@@ -13,5 +13,49 @@ class TestPeak(ut.TestCase):
     def setUp(self):
         ...
     
-    def __init__():
+    def test_init(self):
+        ...
+
+
+    def test_from_gbk(self):
+        ...
+
+
+    def test_from_pkl(self):
+        ...
+
+
+    def test_from_accession(self):
+        ...
+
+
+    def test_setup_variables(self):
+        ...
+
+
+    def test_get_dnaa_boxes_with_mismatches(self):
+        ...
+
+
+    def test_calculate_disparity_curves(self):
+        ...
+
+
+    def test_analyse_disparity_curves(self):
+        ...
+
+
+    def test_calculate_Z_scores(self):
+        ...
+
+
+    def test_calculate_D_scores(self):
+        ...
+
+
+    def test_calculate_G_scores(self):
+        ...
+
+
+    def find_oriCs(self):
         ...
