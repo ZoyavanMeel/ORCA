122e7c56055a942a6cb70b46d0e8dbc758c7ce72
ZoyavanMeel <109024871+ZoyavanMeel@users.noreply.github.com>
2023-06-21 12:46:00 +0200

    Update _2_predict_dataset.py

--- Machine_learning/_2_predict_dataset.py
+++ Machine_learning/_2_predict_dataset.py
@@ -9,21 +9,20 @@ from typing import Union
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
 
 from ORCA import ORCA
-from _1_download_dataset import load_data
 import BioFile as bf
 
 
 # Set these before running
-INPUT_PATH     = bf.comp_path('data\output\doric_set_no_model_orca_pkl')
-CSV_OUT_FOLDER = bf.comp_path('data\output\doric_set_no_model_orca_csvs')
-OUT_FILE_PATH  = bf.comp_path('data\output\doric_set_no_model_orca.csv')
-PARALLEL       = True
-CPUS           = os.cpu_count() - 1
+CLUSTER = "/tudelft.net/staff-umbrella/GeneLocations/ZoyavanMeel/ORCA/"
+
+INPUT_PATH     = CLUSTER + 'data/output/doric_set_no_model_orca_pkl'
+CSV_OUT_FOLDER = CLUSTER + 'data/output/doric_set_no_model_orca_csvs'
+OUT_FILE_PATH  = CLUSTER + 'data/output/doric_set_no_model_orca.csv'
+
+CPUS           = 20
 MAX_ORICS      = 10 # Assumption: No more than 10 oriC for a single organism are predicted
 
-EMAIL   = 'no_need_for_a_real@email_address.com'
-API_KEY = None
-MODEL   = None # joblib.load('Machine_Learning/exp_train_model.pkl')
+MODEL = None # joblib.load('Machine_Learning/exp_train_model.pkl')
 
 
 def predict_pkl_to_csv(path: str, csv_path: str, max_oriCs: int):
@@ -65,19 +64,14 @@ def predict_dataset(
         path_folder: str,
         csv_out_folder: str,
         out_file_path: str,
-        parallel: bool,
         cpus: Union[int, None],
         max_oriCs: int
     ):
 
-    paths = os.listdir(path_folder)
-    if parallel:
-        prepped_prediction = partial(predict_pkl_to_csv, csv_path=csv_out_folder, max_oriCs=max_oriCs)
-        with mp.Pool(cpus) as pool:
-            pool.map(prepped_prediction, paths)
-    else:
-        for path in paths:
-            predict_pkl_to_csv(path, csv_out_folder, max_oriCs)
+    paths = [os.path.join(path_folder, path) for path in os.listdir(path_folder)]
+    prepped_prediction = partial(predict_pkl_to_csv, csv_path=csv_out_folder, max_oriCs=max_oriCs)
+    with mp.Pool(cpus) as pool:
+        pool.map(prepped_prediction, paths)
 
     fieldnames = ['accession', 'version', 'seq_len',]
     fieldnames.extend([f'oriC_middle_{i}' for i in range(max_oriCs)])
@@ -93,7 +87,6 @@ if __name__ == '__main__':
         path_folder=INPUT_PATH,
         csv_out_folder=CSV_OUT_FOLDER,
         out_file_path=OUT_FILE_PATH,
-        parallel=PARALLEL, 
         cpus=CPUS,
         max_oriCs=MAX_ORICS
     )
